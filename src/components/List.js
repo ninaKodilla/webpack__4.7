1c3a5cc9b56e6035b9b2eed26f6ab3842ba4134e
npupiec <nkozlowska89@gmail.com>
2018-06-11 13:36:16 +0200

    code for task

---
+++ src/components/List.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const List = props => {
+  const elements = props.list.map(element => <li key={element.id} id={element.id}> {element.text} <button onClick={() => props.remove(element.id)}>Remove</button></li>)
+
+  return (
+    <ul>{elements}</ul>
+  )
+}
+
+export default List;
